9b681d844fa76b464cf0ef3f191ed84fd25b7cd1
karakayali.akif <karakayali.akif@gmail.com>
2015-05-09 14:44:29 +0000

    Engine (start)

---
+++ Engine.java
@@ -0,0 +1,13 @@
+
+public class Engine {
+	
+	private String engineType;
+	private double cyclinderVolume;
+	private int horsePower;
+	public Engine(String engineType, double cyclinderVolume, int horsePower){
+		this.engineType = engineType;
+		this.cyclinderVolume = cyclinderVolume;
+		this.horsePower = horsePower;
+	}
+
+}
