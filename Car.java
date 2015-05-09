5d5b16fdef5f5f9cac63750b37f0e29106ba0dee
karakayali.akif <karakayali.akif@gmail.com>
2015-05-09 14:46:26 +0000

    Car (start)

---
+++ Car.java
@@ -0,0 +1,8 @@
+
+public class Car extends Vehicle {
+	
+	private final String VEHICLE_TYPE = "car";
+	public Car(String brandName, String modelName, Engine engine, int id, int price){
+		super(brandName,modelName, engine, id, price);
+	}
+}
