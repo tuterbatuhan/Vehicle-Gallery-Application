740d108af99a8b35783580503f92de8d1365b21f
karakayali.akif <karakayali.akif@gmail.com>
2015-05-09 14:47:26 +0000

    SUV (start)

---
+++ SUV.java
@@ -0,0 +1,10 @@
+
+public class SUV extends Vehicle {
+
+		
+		private final String VEHICLE_TYPE = "SUV";
+		
+		public SUV(String brandName, String modelName, Engine engine, int id, int price){
+			super(brandName,modelName, engine, id, price);
+		}
+}
