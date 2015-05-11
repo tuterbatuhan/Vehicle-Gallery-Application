effebd6cd1d94b66253d7a80dd543e1c708bd058
karakayali.akif <karakayali.akif@gmail.com>
2015-05-11 20:00:19 +0000

    23:00 galleryowner

--- GalleryMania/src/gallerymania/User/GalleryOwner.java
+++ GalleryMania/src/gallerymania/User/GalleryOwner.java
@@ -5,29 +5,35 @@ import gallerymania.Vehicle.Vehicle;
 
 
 public class GalleryOwner extends User{
-	public final String UserType = "galleryowner";
-	Gallery gallery;
-	public GalleryOwner(String name, String surname, String userName, String password, Gallery gallery){
-		super(name, surname, userName, password);
-		this.gallery = gallery;
-	}
-	public boolean addVehicle(Vehicle v){
-		boolean result = gallery.addVehicle(v);
-		return result;
-	}
-	public boolean removeVehicle(Vehicle v){
-		boolean result = gallery.removeVehicle(v);
-		return result;
-	}
-        public boolean signIn(String userName, String password){
-		if(super.getUserName().equals(userName) && super.getPassword().equals(password))
-			return true;
-		return false;
-	}
-          public String toString()
-        {
-            String result=name+" "+ surname + "'s Gallery: \n " + gallery;
-            
+    public final String USER_TYPE = "galleryowner";
+    Gallery gallery;
+    public GalleryOwner(String name, String surname, String userName, String password, Gallery gallery){
+            super(name, surname, userName, password);
+            this.gallery = gallery;
+    }
+    public boolean addVehicle(Vehicle v){
+            boolean result = gallery.addVehicle(v);
             return result;
-        }
+    }
+    public boolean removeVehicle(Vehicle v){
+            boolean result = gallery.removeVehicle(v);
+            return result;
+    }
+    public Gallery getGallery(){
+    	return gallery;
+    }
+    /*public boolean signIn(String userName, String password){
+            if(super.getUserName().equals(userName) && super.getPassword().equals(password))
+                    return true;
+            return false;
+    }*/
+    public String toString()
+    {
+        String result=name+" "+ surname + "'s Gallery: \n " + gallery;
+        
+        return result;
+    }
+    public String getUserType(){
+    	return USER_TYPE;
+    }
 }
