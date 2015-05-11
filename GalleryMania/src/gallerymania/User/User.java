4c96fcde0197a4123da1a158f9c5889c49531fe1
karakayali.akif <karakayali.akif@gmail.com>
2015-05-11 20:00:47 +0000

    User değişen hali

--- GalleryMania/src/gallerymania/User/User.java
+++ GalleryMania/src/gallerymania/User/User.java
@@ -3,7 +3,7 @@ package gallerymania.User;
 import java.util.ArrayList;
 
 
-public class User {
+public abstract class User {
 	protected String name;
 	protected String surname;
 	private String userName;
@@ -19,5 +19,18 @@ public class User {
 	}
 	protected String getPassword(){
 		return password;
-	}                
+	}
+	protected String getName(){
+		return name;
+	}
+	protected String getSurname(){
+		return surname;
+	}
+	public boolean signIn(String userName, String password){
+        if(this.userName.equals(userName) && this.password.equals(password))
+        	return true;
+        else
+        	return false;
+	}
+	public abstract String getUserType();
 }
